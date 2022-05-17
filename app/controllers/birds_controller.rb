bafad14cc0e2ff64ed4389316eec51bc4547d545
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-05-17 15:38:40 -0700

    commit tree 406f2ba2565d172188ffd513f37433db1af11ee1

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -16,4 +16,18 @@ class BirdsController < ApplicationController
     end
   end
 
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
+
+  # def create
+  #   bird = Bird.create(bird_params)
+  #   render json: bird, status: :created
+  # end
+
+  # def bird_params
+  #   params.permit(:name, :species)
+  # end
+
 end
